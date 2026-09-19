95d0d9034e421609687788972ab40c61bdc07093
Andrew Fleetwood-Bird <andrew.fleetwood-bird@cloutive.com>
2026-09-19 12:16:19 +0200

    Deploy Lambda services with remote S3 state backend

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "cloutive_personal_account_id" {
-  description = "AWS Account ID for cloutive-personal (to prevent accidental deployment to wrong account)"
-  type        = string
-  validation {
-    condition     = can(regex("^[0-9]{12}$", var.cloutive_personal_account_id))
-    error_message = "Account ID must be a 12-digit number"
-  }
-}
