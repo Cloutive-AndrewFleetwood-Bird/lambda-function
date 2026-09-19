41f24e2d6e1ba7ce8ef9e49f9f969b0e2a0993c9
Andrew Fleetwood-Bird <andrew.fleetwood-bird@cloutive.com>
2026-09-19 11:41:25 +0200

    test

--- ssm.tf
+++ ssm.tf
@@ -1,8 +1,8 @@
 resource "aws_ssm_parameter" "char_counter_url_param" {
   name        = "/cinfra/services/char-counter/url"
-  description = "The WAF-protected ALB URL for the Character Counter Lambda"
+  description = "The URL for the Character Counter Lambda"
   type        = "String"
-  value       = "http://${aws_lb.main.dns_name}/count"
+  value       = aws_lambda_function_url.char_counter_url.function_url
   overwrite   = true 
 }
 
@@ -10,16 +10,16 @@ resource "aws_ssm_parameter" "json_validator_url_param" {
   name        = "/cinfra/services/json-validator/url"
   description = "The WAF-protected ALB URL for the JSON Validator Lambda"
   type        = "String"
-  value       = "http://${aws_lb.main.dns_name}/validate"
+  value       = aws_lambda_function_url.json_validator_url.function_url
   overwrite   = true
 }
 
 output "character_counter_url" {
-  value       = "http://${aws_lb.main.dns_name}/count"
+  value       = aws_lambda_function_url.char_counter_url.function_url
   description = "The direct endpoint for the Character Counter"
 }
 
 output "json_validator_url" {
-  value       = "http://${aws_lb.main.dns_name}/validate"
+  value       = aws_lambda_function_url.json_validator_url.function_url
   description = "The direct endpoint for the JSON Validator"
 }
